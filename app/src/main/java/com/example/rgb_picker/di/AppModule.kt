a7a27f6c5e9628f48c74f76a311cd6dd786c6b0c
viktorkaderabek <viktorkaderabekpc@seznam.cz>
2022-01-30 11:44:27 +0100

    Project

--- app/src/main/java/com/example/rgb_picker/di/AppModule.kt
+++ app/src/main/java/com/example/rgb_picker/di/AppModule.kt
@@ -1,10 +1,38 @@
 package com.example.rgb_picker.di
 
+import com.example.rgb_picker.common.Constants
+import com.example.rgb_picker.data.remote.ThecolorApi
+import com.example.rgb_picker.data.repository.ThecolorRepositoryImpl
+import com.example.rgb_picker.domain.repository.ThecolorRepository
+import com.example.rgb_picker.domain.use_case.GetColorUseCase
 import com.example.rgb_picker.presentation.Color_Detail.ColorDetailViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val AppModule = module {
+
+    fun provideRepository(api: ThecolorApi): ThecolorRepository {
+        return ThecolorRepositoryImpl(api)
+    }
+
+    fun getRetrofitRequest(): ThecolorApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.THECOLORAPI_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ThecolorApi::class.java)
+
+    }
+
+    fun getDeatilUseCase(repository: ThecolorRepository): GetColorUseCase {
+        return GetColorUseCase(repository)
+    }
+
+    single { getDeatilUseCase(get()) }
+    single { provideRepository(get()) }
+    single { getRetrofitRequest() }
 
 }
 
--- app/src/main/java/com/example/rgb_picker/domain/use_case/GetColorUseCase.kt
+++ app/src/main/java/com/example/rgb_picker/domain/use_case/GetColorUseCase.kt
@@ -1,4 +1,25 @@
 package com.example.rgb_picker.domain.use_case
 
-class GetColorUseCase {
+import android.util.Log
+import com.example.rgb_picker.common.Resources
+
+import com.example.rgb_picker.data.remote.dto.toColor
+import com.example.rgb_picker.domain.model.ColorModel
+import com.example.rgb_picker.domain.repository.ThecolorRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+
+class GetColorUseCase(
+    private val repository : ThecolorRepository
+) {
+    operator fun invoke(rgbColor: String): Flow<Resources<ColorModel>> = flow {
+        try {
+            val color = repository.getCoinsByRgb(rgbColor).toColor()
+            emit(Resources.Sucess<ColorModel>(color))
+        }
+        catch (e : Exception){
+            Log.e("message", e.toString())
+        }
+    }
 }
